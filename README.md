93ac4d152069930ca16a22f47f314aeb3c69c97c
ThomasR <trouch@prophesee.ai>
2020-10-14 21:56:43 +0200

    [DEV] create sample

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ Set value:
 Add constraint:
 - LOCK: For a digit, if a line has possibilities only in a block, then the two other blocks contained in this square can't have the value
 - LOCK: For a digit, if a square has possibilities only in a block, then the two other blocks along the line can't have the value
-- PAIR: If a cell has only two possible values, then look for other identical pair of values along the lines or in the square, then other cells in the container can't have these two values
 - FISH: Look for fish patterns (Xwing, Swordfish, JellyFish ...) and add corresponding constraints
+(- PAIR: If a cell has only two possible values, then look for other identical pair of values along the lines or in the square, then other cells in the container can't have these two values)
--- app/main.cpp
+++ app/main.cpp
@@ -1,31 +1,96 @@
+/*********************************************************************************************************************
+ * File : main.cpp                                                                                                   *
+ *                                                                                                                   *
+ * 2020 Thomas Rouch                                                                                                 *
+ *********************************************************************************************************************/
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
-#include "Matrix.h"
 
-using namespace std;
+#include <new_matrix.h>
 
-int main()
+namespace boost_po = boost::program_options;
+namespace bfs = boost::filesystem;
+
+struct Config
 {
-    Matrix game;
-    game.load("../sudoku.csv");
-    game.solve();
-    game.disp();
-    // game.dispAllLinesPossible();
-    //game.dispAllSquaresPossible();
-    // game.dispAllNbrPossible();
-    for (int val = 1; val < 10; val++)
+    std::string input_filename_csv;
+    std::string output_dir_path;
+};
+
+/// @brief Utility function to parse command line attributes
+bool parse_command_line(int argc, char *argv[], Config &config)
+{
+    const std::string short_program_desc(
+        "");
+
+    const std::string long_program_desc(
+        short_program_desc +
+        "");
+
+    boost_po::options_description options_desc;
+    boost_po::options_description base_options("Base options");
+    // clang-format off
+    base_options.add_options()
+        ("help,h", "Produce help message.")
+        ("input-csv,i", boost_po::value<std::string>(&config.input_filename_csv), "Input csv file containing the sudoku grid to solve.")
+        ("output-directory,o", boost_po::value<std::string>(&config.output_dir_path)->default_value("/tmp/sudoku"), "Output directory to export the sudoku solution.")
+        ;
+    // clang-format on
+
+    options_desc.add(base_options);
+
+    boost_po::variables_map vm;
+    try
     {
-        //game.dispGridVal(val);
-        game.doColoring(val - 1);
+        boost_po::store(boost_po::command_line_parser(argc, argv).options(options_desc).run(), vm);
+        boost_po::notify(vm);
     }
-    game.disp();
-    for (int val = 1; val < 10; val++)
+    catch (boost_po::error &e)
     {
-        game.dispGridVal(val);
+        std::cerr << short_program_desc << std::endl;
+        std::cerr << options_desc << std::endl;
+        std::cerr << "Parsing error:" << e.what() << std::endl;
+        return false;
     }
-    game.save();
-    cout << "Hello world!" << endl;
-    while (true)
+
+    if (vm.count("help"))
     {
+        std::cout << short_program_desc << std::endl;
+        std::cout << options_desc << std::endl;
+        return false;
     }
+
+    if (!bfs::exists(config.output_dir_path))
+    {
+        try
+        {
+            bfs::create_directories(config.output_dir_path);
+        }
+        catch (bfs::filesystem_error &e)
+        {
+            std::cerr << "Unable to create folder" << config.output_dir_path << std::endl;
+            return false;
+        }
+    }
+    if (!bfs::exists(config.input_filename_csv))
+    {
+        std::cerr << "The input csv doesn't exist: " << config.input_filename_csv << std::endl;
+        return false;
+    }
+
+    std::cout << long_program_desc << std::endl;
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    Config config;
+    if (!parse_command_line(argc, argv, config))
+        return 1;
+
+    NewMatrix solver;
+    solver.solve_grid(config.input_filename_csv);
     return 0;
 }
