4dd4476a9999915f0fafb0cb0d60cb3039a19fd0
ThomasR <trouch@prophesee.ai>
2020-10-16 21:32:41 +0200

    [DEV] update visu

--- inc/sudoku_displayer.h
+++ inc/sudoku_displayer.h
@@ -21,21 +21,22 @@ public:
     /// @param cells 9x9 grid of cells
     void found_cells(const std::array<NewCell, 81> &cells);
 
+    void candidates_per_cell(const std::array<NewCell, 81> &cells);
+
     void num_candidates_per_cell(const std::array<NewCell, 81> &cells);
 
+    void candidate_cells_containing_val(short val, const std::array<NewCell, 81> &cells);
+
     void num_candidates_per_cell_containing_val(short val, const std::array<NewCell, 81> &cells);
-
-    void counts_on_rows(short val, const std::array<NewLine, 9> &rows);
-
-    void counts_on_cols(short val, const std::array<NewLine, 9> &cols);
 
 private:
     /// @brief Callback to append a character to a stream from a location (i,j)
     using CellToCharCb = std::function<void(std::stringstream &, int, int)>;
 
-    void print_grid(CellToCharCb char_cb) const;
+    void print_grid(CellToCharCb char_cb, bool use_long_line = false) const;
 
-    const char *horizontal_line_str = " _______ _______ _______ ";
+    const char *horizontal_line_str = " _______";
+    const char *long_horizontal_line_str = " _____________________________________";
 };
 
 #endif // SUDOKU_DISPLAYER_H
--- src/sudoku_displayer.cpp
+++ src/sudoku_displayer.cpp
@@ -9,13 +9,17 @@
 #include <sstream>
 #include "sudoku_displayer.h"
 
-void SudokuDisplayer::print_grid(CellToCharCb char_cb) const
+void SudokuDisplayer::print_grid(CellToCharCb char_cb, bool use_long_line) const
 {
     // Value + 1 because the value is shifted to [0;8]
     for (int i = 0; i < 9; i++)
     {
         if (3 * (i / 3) == i) // Multiple of 3
-            std::cout << horizontal_line_str << std::endl;
+        {
+            for (int k = 0; k < 3; k++)
+                std::cout << (use_long_line ? long_horizontal_line_str : horizontal_line_str);
+            std::cout << std::endl;
+        }
         const int i0 = 9 * i;
         std::stringstream ss_row;
         for (int j0 = 0; j0 < 3; j0++)
@@ -29,7 +33,10 @@ void SudokuDisplayer::print_grid(CellToCharCb char_cb) const
         }
         std::cout << ss_row.str() << "|" << std::endl;
     }
-    std::cout << horizontal_line_str << std::endl
+
+    for (int k = 0; k < 3; k++)
+        std::cout << (use_long_line ? long_horizontal_line_str : horizontal_line_str);
+    std::cout << std::endl
               << std::endl;
 }
 
@@ -50,10 +57,44 @@ void SudokuDisplayer::found_cells(const std::array<NewCell, 81> &cells)
     print_grid(char_cb);
 }
 
+void SudokuDisplayer::candidates_per_cell(const std::array<NewCell, 81> &cells)
+{
+    static int count = 0;
+
+    if (count > 12)
+        return;
+
+    std::cout << count++ << ") "
+              << "Number of possible candidates in each cell" << std::endl;
+
+    const auto char_cb = [&cells](std::stringstream &ss, int i, int j) {
+        const auto &cell = cells[9 * i + j];
+        if (cell.is_set())
+        {
+            ss << "           ";
+            return;
+        }
+
+        ss << '(';
+        for (int val = 0; val < 9; val++)
+        {
+            if (cell.is_value_possible(val))
+                ss << val;
+            else
+                ss << " ";
+        }
+        ss << ")";
+    };
+
+    print_grid(char_cb, true);
+}
+
 void SudokuDisplayer::num_candidates_per_cell(const std::array<NewCell, 81> &cells)
 {
     static int count = 0;
 
+    if (count > 12)
+        return;
 
     std::cout << count++ << ") "
               << "Number of possible candidates in each cell" << std::endl;
@@ -64,6 +105,25 @@ void SudokuDisplayer::num_candidates_per_cell(const std::array<NewCell, 81> &cel
             ss << " ";
         else
             ss << cell.get_nbr_candidates();
+    };
+
+    print_grid(char_cb);
+}
+
+void SudokuDisplayer::candidate_cells_containing_val(short val,
+                                                     const std::array<NewCell, 81> &cells)
+{
+    static int count = 0;
+    std::cout << count++ << ") "
+              << "Candidate cells that could take the value "
+              << val + 1 << std::endl;
+
+    const auto char_cb = [&cells, &val](std::stringstream &ss, int i, int j) {
+        const auto &cell = cells[9 * i + j];
+        if (!cell.is_set() && cell.is_value_possible(val))
+            ss << "x";
+        else
+            ss << " ";
     };
 
     print_grid(char_cb);
@@ -83,43 +143,6 @@ void SudokuDisplayer::num_candidates_per_cell_containing_val(short val,
             ss << cell.get_nbr_candidates();
         else
             ss << " ";
-    };
-
-    print_grid(char_cb);
-}
-
-void SudokuDisplayer::counts_on_rows(short val, const std::array<NewLine, 9> &rows)
-{
-    static int count = 0;
-
-    std::cout << count++ << ") "
-              << "Possibilities on row for value "
-              << val + 1 << std::endl;
-
-    const auto char_cb = [&rows, &val](std::stringstream &ss, int i, int j) {
-        const auto &bitset = rows[i].get_bitset(val);
-        if (bitset.none())
-            ss << " ";
-        else
-            ss << bitset[j];
-    };
-
-    print_grid(char_cb);
-}
-
-void SudokuDisplayer::counts_on_cols(short val, const std::array<NewLine, 9> &cols)
-{
-    static int count = 0;
-    std::cout << count++ << ") "
-              << "Possibilities on cols for value "
-              << val + 1 << std::endl;
-
-    const auto char_cb = [&cols, &val](std::stringstream &ss, int i, int j) {
-        const auto &bitset = cols[j].get_bitset(val);
-        if (bitset.none())
-            ss << " ";
-        else
-            ss << bitset[i];
     };
 
     print_grid(char_cb);
